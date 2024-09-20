360284f6054b8a89429d3394d106827e83514bde
ninggiangboy <ninggiangboy@gmail.com>
2024-09-20 16:22:35 +0700

    chore: config max-size of upload file

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 export const generateFormSchema = z.object({
   files: z
     .array(
-      z.instanceof(File).refine((file) => file.size < 4 * 1024 * 1024, {
+      z.instanceof(File).refine((file) => file.size < 2 * 1024 * 1024, {
         message: "File size must be less than 4MB",
       })
     )
     .min(1, {
       message: "At least 1 file is required",
     })
-    .max(5, {
-      message: "Maximum 5 files are allowed",
+    .max(3, {
+      message: "Maximum 3 files are allowed",
     }),
   title: z.string().max(160),
   prompt: z.string().max(160).optional(),
