7e91a6df5c33297f47a799168e655d124f2386e5
Ha Duy Khanh <ninggiangboy@gmail.com>
2024-10-11 20:56:56 +0700

    feat: temporarily disable middleware check auth

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isProtectedRoute = createRouteMatcher(["/quiz(.*)", "/slides(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  // if (isProtectedRoute(req)) auth().protect();
 });
 export const config = {
   matcher: [
